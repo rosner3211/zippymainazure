ecc36609b92cc17f4fa7ad41086e7c70369a0ff0
rosner3211 <83396708+rosner3211@users.noreply.github.com>
2021-05-24 15:06:21 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
-from flask import Flask
+from flask import Flask, redirect
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return redirect("https://zippyloan.com/", code=302)
